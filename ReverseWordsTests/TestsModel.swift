bb15d1ffde479b9d236084f4329dbf865cf30514
JJefro <jevjefro@gmail.com>
2021-09-07 21:20:31 +0300

    Tests update

---
+++ ReverseWordsTests/TestsModel.swift
@@ -0,0 +1,32 @@
+//
+//  TestModel.swift
+//  ReverseWordsTests
+//
+//  Created by Jevgenijs Jefrosinins on 07/09/2021.
+//
+
+import Foundation
+
+struct TestModel {
+    
+    let buttonID = "button"
+    
+    let strExample1 = "Reverse words"
+    let expectedOutput1 = "esreveR sdrow"
+    
+    let strExample2 = "Test string"
+    let expectedOutput2 = "tseT gnirts"
+    
+    let strExample3 = "May the force be with you"
+    let expectedOutput3 = "yaM eht ecrof eb htiw uoy"
+    
+    let strExample4 = """
+        Blackbird singing in the dead of night Take these broken wings and learn to fly All your life You were only waiting for this moment to arise
+        """
+    let expectedOutput4 = """
+        dribkcalB gnignis ni eht daed fo thgin ekaT eseht nekorb sgniw dna nrael ot ylf llA ruoy efil uoY erew ylno gnitiaw rof siht tnemom ot esira
+        """
+    
+    
+    
+}
---
+++ ReverseWordsUITests/UITestsModel.swift
@@ -0,0 +1,8 @@
+//
+//  UITestsModel.swift
+//  ReverseWordsUITests
+//
+//  Created by Jevgenijs Jefrosinins on 07/09/2021.
+//
+
+import Foundation
